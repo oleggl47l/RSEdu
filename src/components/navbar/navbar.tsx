12df74dc32c763821aebac4374e99a3d20bc481d
olegg <s.gelo2004@gmail.com>
2024-03-24 19:14:28 +0300

    nav bar changes

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,11 +4,25 @@ import './navBarStyle.css';
 export const Navbar = () => {
     return (
         <BootstrapNavbar className="custom-navbar" variant={"dark"}>
-        <Container>
-            <BootstrapNavbar.Brand href="#home">ТУТ ЛОГО</BootstrapNavbar.Brand>
+        <Container style={{margin: 'auto'}}>
+
+                <BootstrapNavbar.Brand href="#home">
+                    <img
+                        src={"src/images/text.svg"}
+                        alt="Image robot"
+                        className="d-inline-block align-content-center custom-logo"
+                        style={{ width: '150px' }}
+                    />
+                    <img
+                        src={"src/images/robot.svg"}
+                        alt="Image robot"
+                        className="d-inline-block align-content-center custom-logo"
+                        style={{ width: '60px', marginRight: 'auto'}}
+                    />
+                </BootstrapNavbar.Brand>
             <BootstrapNavbar.Toggle aria-controls="responsive-navbar-nav"/>
             <BootstrapNavbar.Collapse id="responsive-navbar-nav">
-                <Nav className="m-auto">
+                <Nav className="align-self-auto">
                     <Nav.Link href="#about">About</Nav.Link>
                     <Nav.Link href="#downloads">Downloads</Nav.Link>
                     <NavDropdown title="Documentation" id="collapsible-nav-dropdown">
